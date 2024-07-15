251aed16a4b36e9e8e06b1528e53f0239e7b47bf
nguyenvanvu2004 <150237481+nguyenvanvu2004@users.noreply.github.com>
2024-07-15 18:22:23 +0700

    Add files via upload

---
+++ 2024_07_15_095137_create_chi_tiet_don_hangs.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chi_tiet_don_hangs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('id_don_hang'); 
+            $table->unsignedInteger('id_san_pham');
+            $table->unsignedInteger('so_luong');
+            $table->unsignedInteger('tong_gia'); 
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chi_tiet_don_hangs');
+    }
+};
